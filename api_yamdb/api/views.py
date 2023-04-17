0386a2c2ff5ab35399925e536210dd3d37b9d316
Maksim Matveev <impossible14@list.ru>
2023-04-17 16:51:08 +0300

    create views for reviews and comments

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from reviews.models import Review, Comment, Title
+from .serializers import ReviewsSerializer, CommentSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewsSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Review.objects.filter(title=self.kwargs.get('title_id'))
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        return Comment.objects.filter(review=self.kwargs.get('review_id'))
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
