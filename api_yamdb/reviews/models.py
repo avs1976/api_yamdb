beb44a22f9bb2c2973cbf452e2f2bb4e8a1ac024
Максим Матвеев <impossible14@list.ru>
2023-04-18 16:45:21 +0300

    update

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -131,7 +131,7 @@ class Review(models.Model):
     pub_date = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Дата публикации',
-        db_index=True,
+        db_index=True
     )
 
     class Meta:
