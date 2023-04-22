9384cf31d42111f1ee6deb95f439ff33f25a5693
Алексей Шевцов <for-web-reg@yandex.ru>
2023-04-22 21:42:00 +0300

    создан пробный файл для CVS

--- api_yamdb/reviews/management/commands/loaddata.py
+++ api_yamdb/reviews/management/commands/loaddata.py
@@ -1,7 +1,21 @@
 import csv
-
+import os
+from django.conf import settings
 from django.core.management.base import BaseCommand
+
+# from reviews.models import Category
 
 
 class Command(BaseCommand):
-    
+    help = 'Load data from csv files into database'
+
+    def handle(self, *args, **okwargs):
+        # Load data into Categories
+        with open('static/data/category.csv', 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                id, name, slug = row
+                # Category.objects.get_or_create(name=name, slug=slug)
+                print(row)
+
+            self.stdout.write(self.style.SUCCESS('Data loaded successfully'))
