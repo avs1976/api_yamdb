1bce606f51996cfe53f108257bc52dfee611dfb8
ChiffaFox48 <chiffafox48@yandex.ru>
2023-04-22 23:26:33 +0300

    подправила юзеров, и уже не понимаю, чего тест хочет от меня

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, GenreViewSet, ReviewViewSet, TitleViewSet
+from .views import (CategoryViewSet, GenreViewSet, ReviewViewSet,
+                    UserViewSet, TitleViewSet)
 
 app_name = 'api'
 
@@ -13,7 +14,9 @@ v1_router.register('titles', TitleViewSet, basename='titles')
 v1_router.register(
     r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
 )
+v1_router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
+    path('v1/auth/', include('users.urls')),
 ]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,48 +1,6 @@
-from django.contrib.auth.models import AbstractUser
+from users.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
-
-class User(AbstractUser):
-    """Определение пользователей"""
-    USER = 'user'
-    MODERATOR = 'moderator'
-    ADMIN = 'admin'
-    USER_LEVELS = (
-        (USER, "User"),
-        (MODERATOR, "Moderator"),
-        (ADMIN, "Admin")
-    )
-
-    role = models.CharField(
-        'Роль',
-        max_length=32,
-        choices=USER_LEVELS,
-        default=USER
-    )
-    bio = models.TextField(
-        'О Себе',
-        max_length=256,
-        blank=True
-    )
-    email = models.EmailField(blank=True, max_length=254)
-    confirmation_code = models.CharField(
-        'Код подтверждения',
-        max_length=100,
-        null=True
-    )
-
-    @property
-    def is_admin(self):
-        return self.role == User.ADMIN or self.is_superuser
-
-    @property
-    def is_moderator(self):
-        return self.role == User.MODERATOR
-
-    @property
-    def is_user(self):
-        return self.role == User.USER
 
 
 class Genre(models.Model):
