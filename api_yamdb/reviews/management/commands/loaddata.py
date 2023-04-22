3b396a8dafb451b4900c0acb494f33528ed5ab20
Алексей Шевцов <for-web-reg@yandex.ru>
2023-04-22 22:34:12 +0300

    пробный код для загрузки в Category

--- api_yamdb/reviews/management/commands/loaddata.py
+++ api_yamdb/reviews/management/commands/loaddata.py
@@ -1,21 +1,21 @@
 import csv
-import os
-from django.conf import settings
+# import os
+# from django.conf import settings
 from django.core.management.base import BaseCommand
 
-# from reviews.models import Category
+from reviews.models import Category
 
 
 class Command(BaseCommand):
     help = 'Load data from csv files into database'
 
-    def handle(self, *args, **okwargs):
+    def handle(self, *args, **kwargs):
         # Load data into Categories
         with open('static/data/category.csv', 'r', encoding='utf-8') as f:
             reader = csv.reader(f)
+            next(reader)
             for row in reader:
                 id, name, slug = row
-                # Category.objects.get_or_create(name=name, slug=slug)
-                print(row)
+                Category.objects.get_or_create(name=name, slug=slug)
 
-            self.stdout.write(self.style.SUCCESS('Data loaded successfully'))
+        self.stdout.write(self.style.SUCCESS('Data loaded successfully'))
