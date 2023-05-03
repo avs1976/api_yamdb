4e19d766779f86d42c0a463a7c1bce52b7892d93
Алексей Шевцов <for-web-reg@yandex.ru>
2023-05-03 20:02:55 +0300

    Исправления по PEP8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,7 +13,6 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Comment, Genre, Review, Title, TitleGenre
 from users.models import User
 
